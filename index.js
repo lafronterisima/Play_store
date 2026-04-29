2f25e80f8f9d73bf38f2f354ddd1baa9aac9dafc
La Fronterisima <159441596+lafronterisima@users.noreply.github.com>
2026-04-29 11:28:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const configApp = {
     {
       "channel_name": "La Fronterísima TV",
       "channel_url": "https://live20.bozztv.com/giatvplayout7/giatv-209411/playlist.m3u8",
-      "channel_description": "<style>p {margin-bottom: 1.2em; text-align: justify; font-size: 13px;}</style><p>La Fronterísima es una plataforma digital moderna...</p>",
+      "channel_description": "<style>p {margin-bottom: 1.2em; text-align: justify; font-size: 13px;}</style><p>La Fronterísima es una plataforma digital moderna que ofrece una propuesta musical diversa, conectando artistas hispanoamericanos y del mundo para brindar una experiencia sonora auténtica, envolvente y llena de emociones.</p>\n <p>Una alternativa musical diseñada para llegar al corazón de cada oyente, con ritmos que inspiran, motivan y acompañan cada momento del día.</p>\n <p>Descubre, comparte y vibra con los contenidos exclusivos, entrevistas, playlists y una programación variada e innovadora, disponible en cualquier lugar y en cualquier dispositivo las 24 horas, para que la música siempre te acompañe sin límites.</p>",
       "channel_thumbnail": "https://i.postimg.cc/3wckw2kF/IMG-20251228-WA0012.jpg",
       "channel_vast_ads_tag_url": ""
     }
@@ -90,4 +90,4 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Bridge de Radio corriendo en puerto ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Bridge de Radio corriendo en puerto ${PORT}`));
